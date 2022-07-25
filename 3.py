2ab465ac6e8ae4bdd05ad40a81381d56c7b6a30c
KristinaGolushkova <107718947+KristinaGolushkova@users.noreply.github.com>
2022-07-25 15:57:02 +0300

    one

---
+++ 3.py
@@ -0,0 +1,10 @@
+x = [2, 3, 1, 3, 4, 7, 4, 2, 8, 4, 2, 1, 7, 3]
+
+con = 0
+
+while con < len(x):
+    x1 = x.count(x[con])
+    if x1 > 1:
+        x[con] = str(x[con]) * x1
+    con += 1
+print(set(x))
